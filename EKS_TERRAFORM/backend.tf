1d8f7c3a74aea6fc1846c913dd818a6f6fdd7508
shubham-chawhan <shubhamchawhan97@gmail.com>
2025-04-09 21:24:40 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "shubham1-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "shubham143-mrcloudbook777" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
